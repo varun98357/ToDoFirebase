5a97f4af33a4ba87b7cc10f1e92c28fd8540e5f8
Varun K <varunk@Varuns-MacBook-Air.local>
2022-05-12 20:40:37 +0530

    Setup Project Done

---
+++ ToDoFirebase/ToDoFirebase/ToDoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoFirebaseApp.swift
+//  ToDoFirebase
+//
+//  Created by Varun K on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
