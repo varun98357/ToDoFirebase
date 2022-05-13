8bedfdf4c3bd0676b59032e15fbd11523ebc856b
Varun K <varunk@Varuns-MacBook-Air.local>
2022-05-13 08:38:29 +0530

    Navigation Link to Sign Up added from Login View

--- ToDoFirebase/ToDoFirebase/View/Authentication/LoginView.swift
+++ ToDoFirebase/ToDoFirebase/View/Authentication/LoginView.swift
@@ -13,43 +13,61 @@ struct LoginView: View {
     @State private var password = ""
     
     var body: some View {
-        ZStack {
-            BackgroundGradientView()
-            
-            VStack {
-                LogoView()
-                    .padding(.bottom, 25)
+        NavigationView {
+            ZStack {
+                BackgroundGradientView()
                 
-                VStack(spacing: 20) {
-                    EmailTextField(text: $email)
+                VStack {
+                    LogoView()
+                        .padding(.bottom, 25)
                     
-                    PasswordSecureField(text: $password, placeholder: "Password")
-                }
-                .padding(.horizontal, 32)
-                
-                HStack {
-                    Spacer()
+                    VStack(spacing: 20) {
+                        EmailTextField(text: $email)
+                        
+                        PasswordSecureField(text: $password, placeholder: "Password")
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Forgot Password")
+                                .foregroundColor(.white)
+                                .font(.system(size: 13, weight: .semibold))
+                                .padding(.top)
+                                .padding(.trailing, 28)
+                        }
+                    }
                     
                     Button {
                         
                     } label: {
-                        Text("Forgot Password")
-                            .foregroundColor(.white)
-                            .font(.system(size: 13, weight: .semibold))
-                            .padding(.top)
-                            .padding(.trailing, 28)
+                        AuthenticateButtonView(text: "Sign In")
+                            .padding()
                     }
-                }
-                
-                Button {
                     
-                } label: {
-                    AuthenticateButtonView(text: "Sign In")
-                        .padding()
+                    Spacer()
+                    
+                    NavigationLink(destination: SignupView()
+                                                    .navigationBarHidden(true)
+                    ) {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 14))
+                            Text("Sign Up")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        .foregroundColor(.white)
+                    }
+                    .padding(.bottom, 16)
+                    
                 }
-                
-                Spacer()
+                .padding(.top, -44)
             }
+            
         }
     }
 }
